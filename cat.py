c8dfc78e1662164102755cf78e6bbd395eddfd07
Junyi Liu <junyi@Junyis-MacBook-Pro.local>
2018-07-22 18:42:00 -0400

    Cat two jupynb

--- cat.py
+++ cat.py
@@ -5,7 +5,6 @@ notebook1_str = notebook1.read()
 
 # Turn string to dictionary (json)
 notebook1_json = json.loads(notebook1_str)
-#print(notebook1_json)
 
 cells1 = notebook1_json['cells']
 
@@ -13,14 +12,20 @@ notebook2 = open('to_add.ipynb')
 notebook2_str = notebook2.read()
 notebook2_json = json.loads(notebook2_str)
 
-#print(notebook2_str)
 cells2 = notebook1_json['cells']
 
-target_cells = cells1 + cells2 # json
+del notebook1_json['cells']  # Remain the metadata (settings) of jupynb. aka delete the cells data.
 
-target_string = json.dumps(target_cells)
+target_cells = cells1 + cells2  # cat the two json
+
+target_notebook = {}
+
+target_notebook['cells'] = target_cells  # Add elements to the dictionary
+
+target_notebook.update(notebook1_json)  # Add the settings in  notebook1_json to target_notebook
+
+target_string = json.dumps(target_notebook)  # dumps() turn json into string
 
 target = open('target_notebook.ipynb', 'w')
 
 target.write(target_string)
-#print(target_cells)
